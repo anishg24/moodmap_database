64832af61c422e44d7bac50f422b8afa5dc41657
Anish Govind <anishg24@gmail.com>
2018-02-11 11:42:49 -0800

    Update Short-Users.sql

--- Short-Users.sql
+++ Short-Users.sql
@@ -4,10 +4,9 @@ drop table if exists s_users;
 create table s_users (
 	suser_id integer(2) not null primary key,
 	susername varchar(100),
-    suser_pass varchar(100),
+        suser_pass varchar(100),
 );
 
-/*Change the hs_id parameters to hs_id*/
 
 	insert into users (suser_id, susername, suser_pass)
 	values(1, 'anish_the_tall_boi', 'tall_boi');
